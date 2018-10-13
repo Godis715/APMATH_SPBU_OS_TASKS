6da3d857a9647557781fe3fa0613064e13845b4e
Denis Ignatev <godis7.15@gmail.com>
2018-10-14 01:28:01 +0300

    Task controller simple version finished

---
+++ TaskController_1/CPU.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "pdh.h"
+
+
+static PDH_HQUERY cpuQuery;
+static PDH_HCOUNTER cpuTotal;
+
+void init() {
+	PdhOpenQuery(NULL, NULL, &cpuQuery);
+	PdhAddEnglishCounter(cpuQuery, L"\\Processor(_Total)\\% Processor Time", NULL, &cpuTotal);
+	PdhCollectQueryData(cpuQuery);
+}
+
+double getCurrentValue() {
+	PDH_FMT_COUNTERVALUE counterVal;
+
+	PdhCollectQueryData(cpuQuery);
+	PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
+	return counterVal.doubleValue;
+}
